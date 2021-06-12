620e2c5caa40572a7f20e259b9e6e839e1f9b4d4
Youssef1313 <youssefvictor00@gmail.com>
2021-06-12 22:27:30 +0200

    Update search to return node

--- src/RedBlackTree.py
+++ src/RedBlackTree.py
@@ -8,13 +8,20 @@ class RedBlackTree:
 
     def _search(self, node, value):
         if node.value == value:
-            return True
+            return node
         if node.value > value:
-            return node.left is not None and self._search(node.left, value)
-        return node.right is not None and self._search(node.right, value)
+            if node.left is None:
+                return node
+            return self._search(node.left, value)
+
+        if node.right is None:
+            return node
+        return self._search(node.right, value)
 
     def search(self, value):
-        return self.root is not None and self._search(self.root, value)
+        return self.root is not None and\
+               self._search(self.root, value).value == value
+
 
     def insert(self, value):
         if self.root is None:
