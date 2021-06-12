7925df258a41061b078844c471e587db8a1dd504
Youssef1313 <youssefvictor00@gmail.com>
2021-06-12 08:52:43 +0200

    Move search to tree

--- src/RedBlackTree.py
+++ src/RedBlackTree.py
@@ -5,8 +5,15 @@ class RedBlackTree:
     def __init__(self):
         self.root = None
 
+    def _search(self, node, value):
+        if node.value == value:
+            return True
+        if node.value > value:
+            return node.left is not None and self._search(node.left, value)
+        return node.right is not None and self._search(node.right, value)
+
     def search(self, value):
-        return self.root is not None and self.root.search(value)
+        return self.root is not None and self._search(self.root, value)
 
     def insert(self, value):
         if self.root is None:
