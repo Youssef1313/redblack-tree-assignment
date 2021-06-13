aa703ec3d978a201f7cfb643cae3a3e3c02f392f
mohamed-zayton <mohamed-zayton>
2021-06-13 17:29:47 +0200

    run_code

--- src/main.py
+++ src/main.py
@@ -1,5 +1,34 @@
+from RedBlackTree import RedBlackTree
 # Read file Data
 words = []
+tree = RedBlackTree()
 with open("EN-US-Dictionary.txt") as f:
     for word in f:
-        words.append(word.rstrip('\n'))
+        tree.insert(word.rstrip('\n'))
+
+while True:
+
+    print('Press 1 to insert a word')
+    print('Press 2 to print a size')
+    print('Press 3 to print a height')
+    print('Press 4 to search')
+    print('Press 0 to save & exit')
+    option = input('Your option : ')
+    if option == '1':
+        w = input('Enter the word : ')
+        try:
+            tree.insert(w)
+        except Exception as e:
+            print(e)
+    elif option == '2':
+        print(tree.get_size())
+    elif option == '3':
+        print(tree.get_height())
+    elif option == '4':
+        w = input('Enter the word to search : ')
+        try:
+            print(tree.search(w))
+        except Exception as e:
+            print(e)
+    else:
+        break
