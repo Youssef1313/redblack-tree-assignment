65e782e7a4843be1b0d380055a66f92870af5a3f
mohamed-zayton <47240722+mohamed-zayton@users.noreply.github.com>
2021-06-13 17:55:15 +0200

    Read dictionary file and ask user for operations (#15)
    * file_read
    
    * run_code
    
    * run_code
    
    * Apply suggestions from code review
    
    Co-authored-by: Youssef Victor <youssefvictor00@gmail.com>
    
    * run_code
    
    Co-authored-by: mohamed-zayton <mohamed-zayton>
    Co-authored-by: Youssef Victor <youssefvictor00@gmail.com>

---
+++ src/main.py
@@ -0,0 +1,32 @@
+from RedBlackTree import RedBlackTree
+# Read file Data
+tree = RedBlackTree()
+with open("EN-US-Dictionary.txt") as f:
+    for word in f:
+        tree.insert(word.rstrip('\n'))
+
+while True:
+
+    print('Press 1 to insert a word')
+    print('Press 2 to print a size')
+    print('Press 3 to print a height')
+    print('Press 4 to search')
+    print('Press 0 to exit')
+    option = input('Your option : ')
+    if option == '1':
+        w = input('Enter the word : ')
+        try:
+            tree.insert(w)
+        except Exception as e:
+            print(e)
+        print(f"Size: {tree.get_size()}")
+        print(f"Height: {tree.get_height()}")
+    elif option == '2':
+        print(f"Size: {tree.get_size()}")
+    elif option == '3':
+        print(f"Height: {tree.get_height()}")
+    elif option == '4':
+        w = input('Enter the word to search : ')
+        print('Found' if tree.search(w) else 'Not Found')
+    else:
+        break
